457cacbb863dfcd0c264fec91f626cd61a419d0f
Few <91719770+Fewlateo@users.noreply.github.com>
2023-06-30 18:44:09 +0700

    Test02

--- main.swift
+++ main.swift
@@ -1,21 +1,34 @@
 import Foundation
 
-func printStar() {
+func printPattern() {
+    let num = 6
+    let rows = 5
+    let stars = "*"
+    let space = " "    
     
-    let pattern = [
-        [" ", " ", "*", " ", " "],
-        [" ", "*", " ", "*", " "],
-        ["*", "*", " ", "*", "*"],
-        [" ", "*", "*", "*", " "],
-        ["*", " ", " ", " ", "*"]
-    ]
-    
-    for row in pattern {
-        for element in row {
-            print(element, terminator:"")
+    for x in 1...rows {
+        if x == 1 || x == 2 || x == 5 {
+            let spaceCount = num - x
+            let starCount = (2 * x) - 1
+            let leftSpaces = String(repeating: " ", count: spaceCount)
+            if starCount > 2 {
+                let middleSpaces = String(repeating: " ", count: starCount - 2)
+                print(leftSpaces + stars + middleSpaces + stars)
+            } 
+            else {
+                print(leftSpaces + "*")
+            }
         }
-        print()
+        else if x == 3 {
+            let wing = String(stars + space + stars)
+            print(wing + String(repeating: space, count: x+2) + wing)
+        }
+        else if x == 4 {
+            let leftSpaces = String(repeating: space, count: 3)
+            let middle = stars + space
+            print(leftSpaces + String(repeating: middle, count: 3))
+        }
     }
 }
 
-printStar()
+printPattern()
